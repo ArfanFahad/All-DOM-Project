32fac85017fab1d2c4d5589d4f6593f2375bf334
Md Fahad Farazi <arfanfahad40@gmail.com>
2024-08-26 00:13:47 +0600

    ADD a new Folder Day 4 - Stopwatch Timer

---
+++ Day 4 - Stopwatch Timer/script.js
@@ -0,0 +1,13 @@
+
+
+const minuteLabel = document.querySelector("#minutes"); 
+const secondLabel = document.querySelector("#seconds"); 
+const millisecondLabel = document.querySelector("#milliseconds"); 
+
+const startButton = document.querySelector("#startBtn"); 
+const stopButton = document.querySelector("#stopBtn"); 
+const pauseButton = document.querySelector("#pauseBtn"); 
+const resetButton = document.querySelector("#resetBtn"); 
+
+
+const lapList = document.querySelector("#laplist"); 
